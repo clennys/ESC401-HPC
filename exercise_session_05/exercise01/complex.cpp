0a4a2b48c7e2ba91fab45ed73c970c2c6188af2d
Dennys <dennys.huber@pm.me>
2022-03-25 19:17:50 +0100

    code ex02

---
+++ exercise_session_05/exercise01/complex.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stdio.h>
+
+#define SIZE 100
+
+typedef struct complex_vector{
+	int img[SIZE];
+	int re[SIZE];
+} complex_vector;
+
+int linear_combination(complex_vector* x, complex_vector* y, int a, complex_vector* z){
+	for (int i = 0; i < SIZE; i++) {
+		z->re[i] = x->re[i] + a * y->re[i]; 
+		z->img[i] = x->img[i] + a * y->img[i]; 
+	}
+
+	return 0;
+}
+
+int main (int argc, char *argv[])
+{
+	complex_vector* x = new complex_vector();
+	complex_vector* y = new complex_vector();
+	complex_vector* z = new complex_vector();
+
+	int a = 2;
+
+	for (int i = 0; i < SIZE ; i++) {
+		x->img[i] = i;
+		y->img[i] = i;
+		x->re[i] = i;
+		y->re[i] = i;
+	}
+
+	linear_combination(x, y, a, z);
+	
+	for (int i = 0; i < SIZE ; i++) {
+		printf("%d, RE: %d IMG: %d\n", i, z->re[i], z->img[i]);
+	}
+	
+	return 0;
+}
