60e8e94b3aa315877475831af5fd2a361280d94e
Dennys <dennys.huber@pm.me>
2022-03-25 19:32:55 +0100

    update

--- exercise_session_05/exercise01/complex.c
+++ exercise_session_05/exercise01/complex.c
@@ -1,5 +1,6 @@
-#include <cstdlib>
-#include <iostream>
+// #include <cstdlib>
+// #include <iostream>
+#include <stdlib.h>
 #include <stdio.h>
 
 #define SIZE 100
@@ -20,13 +21,13 @@ int linear_combination(complex_vector* x, complex_vector* y, int a, complex_vect
 
 int main (int argc, char *argv[])
 {
-	complex_vector* x = new complex_vector();
-	complex_vector* y = new complex_vector();
-	complex_vector* z = new complex_vector();
+	// complex_vector* x = new complex_vector();
+	// complex_vector* y = new complex_vector();
+	// complex_vector* z = new complex_vector();
 
-	// complex_vector* x = (complex_vector*) malloc(sizeof(complex_vector));
-	// complex_vector* y = (complex_vector*) malloc(sizeof(complex_vector));
-	// complex_vector* z = (complex_vector*) malloc(sizeof(complex_vector));
+	complex_vector* x = (complex_vector*) malloc(sizeof(complex_vector));
+	complex_vector* y = (complex_vector*) malloc(sizeof(complex_vector));
+	complex_vector* z = (complex_vector*) malloc(sizeof(complex_vector));
 
 	int a = 2;
 
