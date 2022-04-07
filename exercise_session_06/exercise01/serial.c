c71fd543b85f1d26b3c87a7d51de33d05c1dc84b
Dennys Huber <hdennys@daint103.login.cscs.ch>
2022-04-07 11:30:53 +0200

    added output

--- exercise_session_06/exercise01/serial.c
+++ exercise_session_06/exercise01/serial.c
@@ -58,7 +58,7 @@ int main(int argc, char *argv[]){
 
     // first loop
     int maxval = 0;
-	#pragma omp parallel default(none)
+	#pragma omp parallel
 	{
 		for (int i=0;i<num_size;i++) if (numbers[i] > maxval) maxval = numbers[i];
 	}
